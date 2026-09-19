76d68efcce5098478b07ca2cd7fc779d2e205a1a
maydaymushrooms <275828626+maydaymushrooms@users.noreply.github.com>
2026-09-19 15:50:25 -0500

    wip continued

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,19 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig, fontProviders } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://maydaymushroom.coop/'
+    site: 'https://maydaymushroom.coop/',
+    fonts: [{
+        provider: fontProviders.local(),
+        name: "Pincoya Black",
+        cssVariable: "--font-pincoya-black",
+        options: {
+        variants: [{
+            src: ['./src/assets/fonts/PincoyaBlack.otf'],
+            weight: 'normal',
+            style: 'normal'
+        }]
+        }
+    }]
 });
